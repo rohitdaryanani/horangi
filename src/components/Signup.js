9815a66833888659d3fb17b9d50db8c8d722fcf4
Rohit Daryanani <rohitdaryanani@yahoo.com>
2018-07-08 21:35:51 +0800

    react-apollo set up

---
+++ src/components/Signup.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Signup = () => {
+  return (
+    <p>xxxx</p>
+  )
+}
+
+export default Signup;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+    uri: "intense-sierra-67303.herokuapp.com/gql"
+});
+
+const Root = () => (
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
+
